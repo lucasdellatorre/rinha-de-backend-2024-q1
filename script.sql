a5240490bf79356afe64d689bfb90e0a899c910d
lucasdellatorre <lucasdellatorre222@gmail.com>
2024-02-19 19:18:39 -0300

    initial config

---
+++ script.sql
@@ -0,0 +1,13 @@
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    limite INTEGER NOT NULL
+);
+
+INSERT INTO clientes (nome, limite)
+VALUES
+    ('o barato sai caro', 1000 * 100),
+    ('zan corp ltda', 800 * 100),
+    ('les cruders', 10000 * 100),
+    ('padaria joia de cocaia', 100000 * 100),
+    ('kid mais', 5000 * 100);
