67ef6c05858658c1346614275f7d3a92b363da66
lucasdellatorre <lucasdellatorre222@gmail.com>
2024-02-21 16:51:53 -0300

    improve transaction func

--- internal/transacoes/controller.go
+++ internal/transacoes/controller.go
@@ -1,6 +1,7 @@
 package transacoes
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
@@ -9,31 +10,60 @@ import (
 	"strings"
 )
 
-const getClienteById = "SELECT limite, saldo from clientes WHERE cliente_id = $1"
+type Cliente struct {
+	limite int
+	saldo  int
+}
 
-const updateClienteSaldoLimite = "UPDATE clientes SET saldo = $1, limite = $2 WHERE cliente_id = $3"
+const getClienteByIdSQL = "SELECT limite, saldo from clientes WHERE cliente_id = $1"
+
+func (t *TransacaoHandler) findClientById(ctx context.Context, id int) (Cliente, error) {
+	cliente := Cliente{}
+	if err := t.store.QueryRow(getClienteByIdSQL, id).Scan(&cliente.limite, &cliente.saldo); err != nil {
+		return cliente, err
+	}
+	return cliente, nil
+}
+
+const updateClienteSaldoSQL = "UPDATE clientes SET saldo = $1 WHERE cliente_id = $2"
+
+func (t *TransacaoHandler) updateSaldoCliente(ctx context.Context, novoSaldo int, id int) bool {
+	if err := t.store.QueryRow(updateClienteSaldoSQL, novoSaldo, id); err != nil {
+		return false
+	}
+	return true
+}
+
+const createTransacaoSQL = "INSERT INTO transacoes (valor, tipo, descricao, cliente_id) VALUES ($1, $2, $3, $4)"
+
+func (t *TransacaoHandler) createTransacao(ctx context.Context, transacao Transacao) bool {
+	if err := t.store.QueryRow(
+		createTransacaoSQL,
+		transacao.Valor,
+		transacao.Tipo,
+		transacao.Descricao,
+		transacao.ClienteId); err != nil {
+		return false
+	}
+	return true
+}
 
 func (t *TransacaoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithCancel(context.Background())
+	// tx, err := t.store.BeginTx(ctx, nil)
+
 	clientId, err := strconv.Atoi(strings.Split(r.URL.Path, "/")[2])
 
 	if err != nil {
-		fmt.Println("Invalid id:", err)
+		w.WriteHeader(404)
+		w.Write([]byte("Sem id"))
 		return
 	}
 
-	var limite int
-	var saldo int
-
-	if err := t.store.QueryRow(getClienteById, clientId).Scan(&limite, &saldo); err != nil {
-		fmt.Println(err)
-		fmt.Println("Erro ao pegar limite e saldo do cliente")
-	}
+	cliente, err := t.findClientById(ctx, clientId)
 
 	// nota: estudar sobre transacoes, pq nao estou fazendo a validacao pelo banco de dados e em multiplas requisicoes concorrentes
 	// ira ter problemas de concistencia. Qual nivel de consistencia escolher?
-
-	fmt.Println("limite", limite)
-	fmt.Println("saldo", saldo)
 
 	transacao := Transacao{ClienteId: clientId}
 
@@ -41,22 +71,42 @@ func (t *TransacaoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	defer r.Body.Close()
 
+	var newSaldo int
+
 	if transacao.Tipo == "d" {
-		newSaldo := saldo - transacao.Valor
-		newLimite := limite - transacao.Valor
-		if newSaldo < newLimite {
-			fmt.Println("Erro 422")
+		newSaldo = cliente.saldo - transacao.Valor
+		if newSaldo < (cliente.limite * -1) {
+			w.WriteHeader(422)
+			w.Write([]byte("Erro 422"))
+			return
 		}
 
-		if err := t.store.QueryRow(updateClienteSaldoLimite, newSaldo, newLimite, clientId); err != nil {
-			fmt.Println(err)
+		cliente.saldo = newSaldo
+
+		if ok := t.updateSaldoCliente(ctx, newSaldo, clientId); !ok {
+			fmt.Println("Erro ao atualizar novo saldo do cliente")
+			return
 		}
+
+	} else if transacao.Tipo == "c" {
+
 	}
 
-	if err := t.store.QueryRow("INSERT INTO transacoes (valor, tipo, descricao, cliente_id) VALUES ($1, $2, $3, $4)",
-		transacao.Valor, transacao.Tipo, transacao.Descricao, transacao.ClienteId); err != nil {
-		fmt.Println(err)
+	if ok := t.createTransacao(ctx, transacao); !ok {
+		fmt.Println("Erro ao criar transacao")
+		return
 	}
+
+	clienteResponseData, err := json.Marshal(cliente)
+
+	if err != nil {
+		fmt.Println("Erro json marshalling")
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(clienteResponseData)
 }
 
 type TransacaoHandler struct {
